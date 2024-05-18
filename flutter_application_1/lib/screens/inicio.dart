64b723af31b0a2576298fd75400f05944d9ad254
Danielzxc24 <danielmartinez69207@gmail.com>
2024-05-17 20:51:25 -0600

    actualizacion screens

---
+++ flutter_application_1/lib/screens/inicio.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Inicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Bienvenido',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                
+              },
+              child: Text('Libros'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

5ccef82a01ec4d977026dfe44a10dc23880a4690
Danielzxc24 <danielmartinez69207@gmail.com>
2024-05-17 20:38:50 -0600

    actualizacion dependencias y estructura base

---
+++ flutter_application_1/lib/core/appRouter.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  errorPageBuilder: (context, state) {
+    return Scaffold(
+      body: Center(
+        child: Text('Error: ${state.error}'),
+      ),
+    );
+  },
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => Inicio(),
+    ),
+
+  ],
+);
